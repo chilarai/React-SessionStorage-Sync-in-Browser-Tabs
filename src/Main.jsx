c45ebe1a8d9b17ec5aa9331165b0ab94913b71be
Chilarai <chilly5476@gmail.com>
2020-07-10 11:06:02 +0530

    Session managed when opening new tab. Code comments

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,11 +1,86 @@
 import React, { Component } from "react";
 
 class Main extends Component {
+    constructor(props) {
+        super(props);
+
+        // Install an event listener with name "storage"
+        // Whenever there is a change in the local storage,
+        // this event will be fired
+        // and all the open tabs will be notified
+
+        window.addEventListener("storage", () => {});
+    }
+
     componentDidMount = () => {
-        this.newSession();
+        this.changeDetect();
+    };
+
+    changeDetect = () => {
+        // Set a dummy localstorage item and then delete it
+        // Just dummy activity to fire the above event listener
+        // when a new tab is opened
+
+        window.localStorage.setItem(
+            "REQUESTING_STORAGE",
+            Date.now().toString()
+        );
+        window.localStorage.removeItem("REQUESTING_STORAGE");
+
+        // Actual function to be called when a local storage
+        // event change is detected in any opentab
+
+        window.addEventListener("storage", (event) => {
+            // Check if any one of the item is present in sessionStorage
+
+            const session = JSON.parse(
+                window.sessionStorage.getItem("numberA")
+            );
+
+            // CONDITION: Add to Local Storage
+            // If 'session' isnt empty
+            // then set all the values in sessionStorage to localStorage as well
+            // these will be copied in the sessionStorage in new tabs
+
+            if (event.key === "REQUESTING_STORAGE" && session) {
+                console.log("called");
+                window.localStorage.setItem(
+                    "STORAGE_SHARING",
+                    JSON.stringify({
+                        numberA: sessionStorage.getItem("numberA"),
+                        numberB: sessionStorage.getItem("numberB"),
+                    })
+                );
+                window.localStorage.removeItem("STORAGE_SHARING");
+            }
+
+            // CONDITION: Open New Tab
+            // Copy the localStorage data to sessionStorage
+            console.log(event.key, session);
+
+            if (event.key === "STORAGE_SHARING" && !session) {
+                console.log("un called", JSON.parse(event.newValue).numberA);
+                window.sessionStorage.setItem(
+                    "numberA",
+                    JSON.parse(event.newValue).numberA
+                );
+                window.sessionStorage.setItem(
+                    "numberB",
+                    JSON.parse(event.newValue).numberB
+                );
+
+                // Set the new states
+                this.setState({
+                    numberA: JSON.parse(event.newValue).numberA,
+                    numberB: JSON.parse(event.newValue).numberB,
+                });
+            }
+        });
     };
 
     // Generate new session data
+    // on button click
+
     newSession = () => {
         // Set new data in sessionStorage
         sessionStorage.setItem("numberA", Math.random());
@@ -16,6 +91,16 @@ class Main extends Component {
             numberA: sessionStorage.getItem("numberA"),
             numberB: sessionStorage.getItem("numberB"),
         });
+        this.changeDetect();
+    };
+
+    // Delete session
+    // on button click
+
+    deleteSession = () => {
+        sessionStorage.clear();
+        window.localStorage.setItem("STORAGE_FLUSH", Date.now().toString());
+        window.localStorage.removeItem("STORAGE_FLUSH");
     };
 
     // State
@@ -28,16 +113,21 @@ class Main extends Component {
         return (
             <React.Fragment>
                 <h1>SessionStorage data sync in browser tabs</h1>
-                <p>NumberA: {this.state.numberA}</p>
-                <p>NumberB: {this.state.numberB}</p>
+                <p>
+                    <b>Number A:</b> {this.state.numberA}
+                </p>
+                <p>
+                    <b>Number B:</b> {this.state.numberB}
+                </p>
 
                 <button color="danger" onClick={this.newSession}>
                     Generate New Session
                 </button>
+                <button color="danger" onClick={this.deleteSession}>
+                    Delete All Session
+                </button>
                 <p />
-                <a href="/" target="_blank">
-                    Open New Tab
-                </a>
+                <a href="/">Right Click &amp; Select Open New Tab</a>
             </React.Fragment>
         );
     }
